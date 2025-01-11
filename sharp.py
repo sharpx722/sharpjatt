f4bfb8ed44538f07b0c44c94e85fd23e1c29a74e
sharpx722 <sharpx722@gmail.com>
2025-01-11 23:35:28 +0530

    Update sharp.py

--- sharp.py
+++ sharp.py
@@ -21,7 +21,7 @@ redeem_codes_collection = db['redeem_codes']
 attack_logs_collection = db['user_attack_logs']
 
 # Bot Configuration
-TELEGRAM_BOT_TOKEN = '7903565188:AAH2WWj8903AQnEWHx7nzBTH4SZcMn6AAYA'
+TELEGRAM_BOT_TOKEN = '7903565188:AAFTiVyvnobgs_xAqqt0Hj3EJzEhxCoAO9w'
 ADMIN_USER_ID = 1009132250
 ADMIN_USER_ID = 1009132250 
 COOLDOWN_PERIOD = timedelta(minutes=1) 
